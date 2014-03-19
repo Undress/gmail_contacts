cb70e2343559ca8bc38ab9faf01eb00123799b60
Undress <andrejs.proskurins@gmail.com>
2014-03-19 14:50:48 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GmailContacts::Application.config.secret_key_base = 'af0776d74f38c9133fb722c767a57d01fd00b8f8e21769482a3a62f0e275b0dd92bf5e25c4bd66a4cfce9df577f9bea562734aad2fd3cff0aec1dd74bd474b3c'
